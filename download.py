993232cb23b253c20594af72147ffd7a4b0f7b43
Dmitri Martynov <mdylll@mail.ru>
2020-07-08 21:47:40 +0300

    add correcions

--- download.py
+++ download.py
@@ -5,25 +5,28 @@ import time
 from io import BytesIO
 from multiprocessing.pool import ThreadPool
 from pathlib import Path
-from queue import Queue
 
 import requests
 from PIL import Image
 
+
 def check_args(args):
     if not os.path.exists(args.filename):
-        return f"File {args.filename} doesn't exist"
+        raise FileNotFoundError(f"File {args.filename} doesn't exist")
     Path(args.dir).mkdir(parents=True, exist_ok=True)
     if int(args.threads) < 1:
-        return "Number of threads should be integer 1 or more", 0, 0
+        raise ValueError("Number of threads should be integer 1 or more")
     try:
         h, w = args.size.split('x')
-    except ValueError:
-        return "Incorrect size format", 0, 0
+        h = int(h)
+        w = int(w)
+    except ValueError as e:
+        print("Incorrect size format")
+        raise e
     h, w = int(h), int(w)
     if h < 1 or w < 1:
-        return "height and weight should be integer 1 or more", 0, 0
-    return 0, int(h), int(w)
+        raise ValueError("height and weight should be integer 1 or more")
+    return None
 
 
 class PreviewImage:
@@ -42,43 +45,42 @@ class PreviewImage:
         try:
             response = requests.get(self.url)
             img = Image.open(BytesIO(response.content))
-            self.__class__.byte_size += len(response.content)
-            img = img.resize(self.__class__.size)
+            self.byte_size += len(response.content)
+            img = img.resize(self.size)
             img_name = str(self.idx).zfill(5) + '.jpg'
-            img.save(os.path.join(self.__class__.dir, img_name), format='JPEG')
+            img.save(os.path.join(self.dir, img_name), format='JPEG')
             print(f"URL {self.url} was processed. File: {str(self.idx).zfill(5) + '.jpg'}")
         except Exception as e:
             print(f"URL {self.url} was not processed. {e}")
+            lock.acquire()
             self.__class__.file_with_error += 1
+            lock.release()
         else:
+            lock.acquire()
             self.__class__.file_success += 1
+            lock.release()
 
 
 start_time = time.time()
 parser = argparse.ArgumentParser(description='parsing and processing images')
 parser.add_argument('filename', help='file with list of urls')
-parser.add_argument('--dir', default=os.getcwd(), help='folder for images')
-parser.add_argument('--threads', default=1, help='quantity of threads')
-parser.add_argument('--size', default='100x100', help='size of saved images, example: 100x100')
+parser.add_argument('--dir', '-d', default=os.getcwd(), help='folder for images')
+parser.add_argument('--threads', '-t', default=1, help='quantity of threads')
+parser.add_argument('--size', '-s', default='100x100', help='size of saved images, example: 100x100')
 
 args = parser.parse_args()
-print(args.filename)
-check, h, w = check_args(args)
-if check != 0:
-    print(check)
-    raise SystemExit(1)
-
-PreviewImage.size = (h, w)
+check_args(args)
+h, w = [int(el) for el in args.size.split('x')]
+PreviewImage.size = h, w
 PreviewImage.dir = args.dir
-image_queue = Queue()
+image_queue = []
 with open(args.filename) as file:
     for idx, line in enumerate(file):
-        image_queue.put(PreviewImage(line.rstrip('\n'), idx))
+        image_queue.append(PreviewImage(line.rstrip('\n'), idx))
 
 
 def ret(q):
-    while not q.empty():
-        task = q.get()
+    for task in q:
         yield task
 
 
